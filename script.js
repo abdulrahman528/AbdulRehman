8dcb0342ca3e50089207b30a64a0d4899e322ecc
abdulrahman528 <abdulrahmansiraiki@gmail.com>
2025-10-18 20:34:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function checkSections() {
 
   sections.forEach(section => {
     const sectionTop = section.getBoundingClientRect().top;
-
     if (sectionTop < triggerBottom) {
       section.classList.add('fade-in');
     }
